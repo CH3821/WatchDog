f0ce4ab3fb13a2de0aeb75da8c8b4204bf45aafe
JackChan <stzn14@gmail.com>
2017-03-29 16:13:22 +0800

    1、增加第三方库MBProgressHUD 2、为BaseViewController增加LoadingHud

--- WatchDog/WatchDog/Common/Base/BaseViewController.swift
+++ WatchDog/WatchDog/Common/Base/BaseViewController.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import MBProgressHUD
+import SnapKit
+
+fileprivate struct BaseVCUX {
+    struct HUD {
+        static let CornerRadius: CGFloat = 8
+        static let MinSize = CGSize(width: 32, height: 32)
+        static let TextColor = UIConstants.Color.TextColor
+        static let Font = UIConstants.Font.Tips
+        static let DefaultText = "请稍后..."
+        private init() {}
+    }
+    
+    private init() {}
+}
 
 class BaseViewController: UIViewController {
 
+    // MARK: - Properties
+    lazy var hud: MBProgressHUD = {
+        let hud = MBProgressHUD()
+        hud.bezelView.layer.cornerRadius = BaseVCUX.HUD.CornerRadius
+        hud.minSize = BaseVCUX.HUD.MinSize
+        hud.label.textColor = BaseVCUX.HUD.TextColor
+        hud.label.font = BaseVCUX.HUD.Font
+        return hud
+    }()
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +42,9 @@ class BaseViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = UIConstants.Color.BackgroundColor
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        view.addSubview(hud)
+        
+        layoutPageSubviews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +69,29 @@ class BaseViewController: UIViewController {
 
 }
 
+// MARK: - Public Methods
+extension BaseViewController: PublicMethods {
+    public func showLoading(message: String? = BaseVCUX.HUD.DefaultText) {
+        if let message = message {
+            hud.label.text = message
+        }
+        hud.show(animated: true)
+    }
+    
+    public func hideLoading() {
+        hud.hide(animated: true)
+    }
+}
+
+// MARK: - Private Methods
+extension BaseViewController: PrivateMethods {
+    fileprivate func layoutPageSubviews() {
+        hud.snp.makeConstraints { (make) in
+            make.center.equalTo(self.view)
+            make.size.greaterThanOrEqualTo(BaseVCUX.HUD.MinSize)
+        }
+    }
+}
 
 extension BaseViewController {
     
